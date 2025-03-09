320cee1641a14b5397a1e8cfe77445caaa3e5bb4
Rahul Aggarwal <rahulagg217@gmail.com>
2025-03-09 20:34:06 +0530

    DSA

---
+++ C++/conditionalAndLoops.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int a = 10;
+    int b = 20;
+
+    if (a>b) {
+        cout<< "a is greater than b" << endl;
+    } else {
+        cout <<"b is greater than a" << endl;
+    }
+}
---
+++ C++/primeNo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int i  = 1, n;
+    cin >> n;
+
+    cout << "value of n is :" << endl;
+//prime number
+    // while(i<=n) {
+    //    
+    // }
+}
